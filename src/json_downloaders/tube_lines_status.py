b98c5345755ab9a076320c51ba30d8bd8d9deeb4
Adam Barwell <a.barwell@cs.ucl.ac.uk>
2011-08-10 15:35:46 +0100

    Changed the downloader for rating averages, now is (hopefully) more efficient and offers meta information

--- src/json_downloaders/tube_lines_status.py
+++ src/json_downloaders/tube_lines_status.py
@@ -1,19 +1,23 @@
 from data.check_in_db import check_in_db_key
 from data.strings import tube_lines
+from data.user_db import user_db
 from datetime import datetime
 from django.utils import simplejson as json
 from google.appengine.ext import db, webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
-import logging
 
 class main_page(webapp.RequestHandler):
+    present = datetime.now()
+    one_hours_past = present.replace(hour=(present.hour-1))
+    three_hours_past = present.replace(hour=(present.hour-3))
+    
     def get(self):
         self.response.headers['Content-Type'] = "application/json"
         printable_status = dict.fromkeys(tube_lines)
         for index in range(len(tube_lines)):
-            gotten_average = self.get_average(tube_lines[index], 0)
+            gotten_average = self.get_average(tube_lines[index], False)
             if gotten_average == -1:
-                printable_status[tube_lines[index]] ='No Ratings Available'
+                printable_status[tube_lines[index]] = None
             else:
                 printable_status[tube_lines[index]] = gotten_average
         
@@ -21,15 +25,10 @@ class main_page(webapp.RequestHandler):
     
     def get_average(self, line, threehours):
         "Returns entries from the database, depending on the given line"
-        present = datetime.now()
-        logging.info(present)
-        
-        if threehours == 1:
-            logging.info('getting one hour')
-            past = present.replace(hour=(present.hour-1))
+        if threehours is False:
+            past = self.one_hours_past
         else:
-            logging.info('getting three hours')
-            past = present.replace(hour=(present.hour-3))
+            past = self.three_hours_past
             
         time_query = db.GqlQuery("SELECT * "
                          "FROM check_in_db "
@@ -40,40 +39,37 @@ class main_page(webapp.RequestHandler):
                          line)
 
         entries = time_query.fetch(500)
-        logging.info('length of entries')
-        logging.info(len(entries))
         
         if (len(entries) == 0):
-            if (threehours == 0):
-                return self.get_average(line, 1)
+            if (threehours is False):
+                return self.get_average(line, True)
             else:
-                logging.info('returning -1')
                 return -1
         else:
-            if (len(entries) < 10):
-                if (threehours == 0):
-                    return self.get_average(line, 1)
-                else:
-                    return self.compute_averages(entries)
+            if (len(entries) < 10) and (threehours is False):
+                return self.get_average(line, True)
             else:        
-                return self.compute_averages(entries)
+                return self.compute_averages(entries, threehours)
 
-    def compute_averages(self, entries):
+    def compute_averages(self, entries, threehours):
         average_rating_delay = 0
         average_rating_crowded = 0
         average_rating_happiness = 0
+        users = []
         for entry in entries:
             average_rating_delay += entry.rating_delay
             average_rating_crowded += entry.rating_crowded
             average_rating_happiness += entry.rating_happiness
-            logging.info(average_rating_delay)
-            logging.info(average_rating_crowded)
-            logging.info(average_rating_happiness)
+            if not (entry.user.key() in users):
+                users.append(entry.user.key())
             
         return dict({
                      'rating':average_rating_delay/len(entries), 
                      'crowded':average_rating_crowded/len(entries), 
-                     'happiness':average_rating_happiness/len(entries)
+                     'happiness':average_rating_happiness/len(entries),
+                     'users':len(users),
+                     'threehours':threehours,
+                     'entries':len(entries)
                      })
 
 def main():
